3ca01b841c1038873c6b6d65cec62315237a5381
Osama <0xm.osamaa@gmail.com>
2023-05-18 18:09:18 +0300

    replaced lorem

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -17,11 +17,17 @@ const LandingPage = () => {
                     </div>
                     <div className="description col-12 col-md-7">
                         <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Vel, incidunt! Voluptas nam, error aut,
-                            eveniet vel blanditiis facilis fugit explicabo nihil
-                            quaerat ad consequatur deserunt libero magnam
-                            aspernatur ratione voluptatibus.
+                            I’m a Software Engineer with experience in designing
+                            the frontend and implementing the backend of your
+                            next favorite website. Whether you’re trying to
+                            kickstart your business, list your services or
+                            create an online store – I will help!
+                        </p>
+                        <p>
+                            I'm very flexible with the techs I use and have no
+                            problem in adapting to your project's needs. I can
+                            and will deliver great results with process that's
+                            timely and collaborative with great value.
                         </p>
                     </div>
                     <div className="row my-3">
@@ -44,7 +50,13 @@ const LandingPage = () => {
                                 <h5>Laravel</h5>
                             </li>
                             <li>
-                                <h5>Nodejs</h5>
+                                <h5>Nodejs/Expressjs</h5>
+                            </li>
+                            <li>
+                                <h5>MySQL</h5>
+                            </li>
+                            <li>
+                                <h5>Mongodb</h5>
                             </li>
                             <li>
                                 <h5>HTML</h5>
@@ -63,8 +75,16 @@ const LandingPage = () => {
                             <ProgressBar striped variant="success" now={95} />
                         </div>
                         <div className="skill-progress">
-                            <h5>Nodejs</h5>
+                            <h5>Nodejs/Expressjs</h5>
                             <ProgressBar striped variant="success" now={80} />
+                        </div>
+                        <div className="skill-progress">
+                            <h5>MySQL</h5>
+                            <ProgressBar striped variant="success" now={90} />
+                        </div>
+                        <div className="skill-progress">
+                            <h5>Mongodb</h5>
+                            <ProgressBar striped variant="success" now={75} />
                         </div>
                         <div className="skill-progress">
                             <h5>HTML</h5>
